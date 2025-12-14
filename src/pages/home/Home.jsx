066f2bbfc0822e1b78282b6d6ef91674ff593770
vatsal panchal <panchalvatsal2025@gmail.com>
2025-12-14 21:27:46 +0530

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -46,9 +46,9 @@ const Home = () => {
           <div className="flex items-center justify-center animate-slide-up-delay-3 px-4">
             {/* <a href="/icons"> */}
             <Link to='/Icons'>
-               <button className="group relative px-6 sm:px-5 py-2.5 sm:py-3 bg-white text-black font-semibold rounded-lg overflow-hidden transition-all duration-300 hover:scale-105 hover:shadow-2xl hover:shadow-white/20 text-sm sm:text-base">
+               <button className="group relative px-5 sm:px-5 py-2.5 sm:py-3 bg-white text-black font-semibold rounded-lg overflow-hidden transition-all duration-300 hover:scale-105 hover:shadow-2xl hover:shadow-white/20 text-sm sm:text-base">
                 <span className="relative z-10 flex items-center gap-2 cursor-pointer">
-               get started
+              Explore Icons
                 </span>
               </button>
             </Link>
