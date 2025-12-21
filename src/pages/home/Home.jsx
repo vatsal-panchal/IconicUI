4273d3e6d408240e871295fa7b93d150b9bb1aaa
vatsal panchal <panchalvatsal2025@gmail.com>
2025-12-21 11:58:05 +0530

    code

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,8 @@
+
 import React from "react";
-import { Link } from "react-router-dom";
-import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
+import { Link } from "react-router-dom";
+import './Home.css'
 
 const Home = () => {
   return (
@@ -21,14 +22,15 @@ const Home = () => {
         ))}
       </div>
 
-      <Navbar/>
-
       <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-black/80 to-black z-0" />
       <div className="absolute top-0 left-1/4 w-64 h-64 sm:w-96 sm:h-96 bg-blue-500/10 rounded-full blur-3xl animate-pulse-slow" />
       <div className="absolute bottom-0 right-1/4 w-64 h-64 sm:w-96 sm:h-96 bg-purple-500/10 rounded-full blur-3xl animate-pulse-slower" />
 
+      <Navbar />
+
       <main className="relative z-10">
         <section className="max-w-5xl mx-auto px-4 sm:px-6 pt-16 sm:pt-24 md:pt-32 pb-12 sm:pb-20 text-center">
+          {/* Badge */}
           <div className="inline-flex items-center gap-2 px-3 sm:px-4 py-1.5 sm:py-2 rounded-full bg-white/5 border border-white/10 backdrop-blur-sm mb-6 sm:mb-8 animate-fade-in">
             <span className="relative flex h-2 w-2">
               <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
@@ -40,10 +42,7 @@ const Home = () => {
           </div>
 
           <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl xl:text-[5vw] font-semibold mb-4 sm:mb-5 leading-tight animate-slide-up px-4">
-            <span
-              className="glitch-text inline-block bg-gradient-to-r from-white via-gray-300 to-gray-800 bg-clip-text text-transparent"
-              data-text="Build Beautiful UI Faster"
-            >
+            <span className="glitch-text inline-block bg-gradient-to-r from-white via-gray-300 to-gray-800 bg-clip-text text-transparent" data-text="Build Beautiful UI Faster">
               Build Beautiful UI Faster
             </span>
           </h1>
@@ -57,147 +56,13 @@ const Home = () => {
 
           <div className="flex items-center justify-center animate-slide-up-delay-3 px-4">
             <Link to="/Icons">
-              <button className="star-button ">
+              <button className="button42">
                 Explore Icons
-                <div className="star-1">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlSpace="preserve"
-                    version="1.1"
-                    style={{
-                      shapeRendering: "geometricPrecision",
-                      textRendering: "geometricPrecision",
-                      imageRendering: "optimizeQuality",
-                      fillRule: "evenodd",
-                      clipRule: "evenodd"
-                    }}
-                    viewBox="0 0 784.11 815.53"
-                  >
-                    <g>
-                      <path
-                        className="fil0"
-                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-                      />
-                    </g>
-                  </svg>
-                </div>
-                <div className="star-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlSpace="preserve"
-                    version="1.1"
-                    style={{
-                      shapeRendering: "geometricPrecision",
-                      textRendering: "geometricPrecision",
-                      imageRendering: "optimizeQuality",
-                      fillRule: "evenodd",
-                      clipRule: "evenodd"
-                    }}
-                    viewBox="0 0 784.11 815.53"
-                  >
-                    <g>
-                      <path
-                        className="fil0"
-                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-                      />
-                    </g>
-                  </svg>
-                </div>
-                <div className="star-3">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlSpace="preserve"
-                    version="1.1"
-                    style={{
-                      shapeRendering: "geometricPrecision",
-                      textRendering: "geometricPrecision",
-                      imageRendering: "optimizeQuality",
-                      fillRule: "evenodd",
-                      clipRule: "evenodd"
-                    }}
-                    viewBox="0 0 784.11 815.53"
-                  >
-                    <g>
-                      <path
-                        className="fil0"
-                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-                      />
-                    </g>
-                  </svg>
-                </div>
-                <div className="star-4">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlSpace="preserve"
-                    version="1.1"
-                    style={{
-                      shapeRendering: "geometricPrecision",
-                      textRendering: "geometricPrecision",
-                      imageRendering: "optimizeQuality",
-                      fillRule: "evenodd",
-                      clipRule: "evenodd"
-                    }}
-                    viewBox="0 0 784.11 815.53"
-                  >
-                    <g>
-                      <path
-                        className="fil0"
-                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-                      />
-                    </g>
-                  </svg>
-                </div>
-                <div className="star-5">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlSpace="preserve"
-                    version="1.1"
-                    style={{
-                      shapeRendering: "geometricPrecision",
-                      textRendering: "geometricPrecision",
-                      imageRendering: "optimizeQuality",
-                      fillRule: "evenodd",
-                      clipRule: "evenodd"
-                    }}
-                    viewBox="0 0 784.11 815.53"
-                  >
-                    <g>
-                      <path
-                        className="fil0"
-                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-                      />
-                    </g>
-                  </svg>
-                </div>
-                <div className="star-6">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlSpace="preserve"
-                    version="1.1"
-                    style={{
-                      shapeRendering: "geometricPrecision",
-                      textRendering: "geometricPrecision",
-                      imageRendering: "optimizeQuality",
-                      fillRule: "evenodd",
-                      clipRule: "evenodd"
-                    }}
-                    viewBox="0 0 784.11 815.53"
-                  >
-                    <g>
-                      <path
-                        className="fil0"
-                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-                      />
-                    </g>
-                  </svg>
-                </div>
               </button>
             </Link>
           </div>
         </section>
       </main>
-
-
     </div>
   );
 };
